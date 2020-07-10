f98cf975ba3d70e9890932fd4fd9a27d32118b5b
Yannick Spreen <yannick.spreen@gmail.com>
2020-07-10 10:00:07 +0200

    Fix path.

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ do
     if [ -d "$filter" ]
     then
         cd "$filter"
-        docker build -t "bio-node-filters/$filter" .
-        docker push "bio-node-filters/$filter"
+        docker build -t "bromberglab/bio-node-filters/$filter" .
+        docker push "bromberglab/bio-node-filters/$filter"
         cd ..
     fi
 done
