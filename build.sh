7625bf0afc900f1d2335cbff37e646d8662d620f
Yannick Spreen <yannick.spreen@gmail.com>
2020-07-10 09:56:49 +0200

    Change to hub action.

--- build.sh
+++ build.sh
@@ -1,10 +1,12 @@
+echo "${DOCKER_PASSWORD}" | docker login -u ${DOCKER_USERNAME} --password-stdin
+
 ls -1 | while read filter
 do
     if [ -d "$filter" ]
     then
         cd "$filter"
-        docker build -t "gcr.io/$GKE_PROJECT/filters/$filter" .
-        docker push "gcr.io/$GKE_PROJECT/filters/$filter"
+        docker build -t "bio-node-filters/$filter" .
+        docker push "bio-node-filters/$filter"
         cd ..
     fi
 done
